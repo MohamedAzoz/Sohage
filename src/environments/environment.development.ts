87f13d609a8bc08ba94877f387e0e0e0a0bd8d05
mohamed azoz <125761608+MohamedAzoz@users.noreply.github.com>
2025-02-19 11:01:16 +0200

    all

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
-  jsonBinApiUrl: 'https://api.jsonbin.io/v3/b/67b59559ad19ca34f8097816',
+  production: false,
+  jsonBinApiUrl: 'http://localhost:3000',
+
 };
